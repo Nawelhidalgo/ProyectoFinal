d49b6c35a331442807f7e08034a7ba63cc7beb21
Nawelhidalgo <nahuelhidalgo112@gmail.com>
2025-07-03 14:40:41 -0300

    funcion para activar o desactivar planta

--- routes/funcionesAdmin.py
+++ routes/funcionesAdmin.py
@@ -11,11 +11,19 @@ def obtener_todos_los_usuarios():
 def obtener_plantas():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT IdPlanta, Nombre, Foto FROM Plantas")
+    cursor.execute("SELECT IdPlanta, Nombre, Foto, Activo FROM Plantas") 
     plantas = cursor.fetchall()
     cursor.close()
     conn.close()
     return plantas
+
+def cambiar_estado_planta(id_planta, estado):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Plantas SET Activo = %s WHERE IdPlanta = %s", (estado, id_planta))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def obtener_comentarios_por_usuario(id_usuario):
     conn = get_db_connection()

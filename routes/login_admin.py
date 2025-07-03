2b7d1a01c7f3f42c060b4a991f6cee4995a72302
Nawelhidalgo <nahuelhidalgo112@gmail.com>
2025-07-03 14:41:20 -0300

    creaacion de rutas activar y desactivar plantas

--- routes/login_admin.py
+++ routes/login_admin.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, request, session, jsonify, redirect, url_for
 from db.connection import get_db_connection
 from werkzeug.security import check_password_hash
-from routes.funcionesAdmin import obtener_todos_los_usuarios
-from routes.funcionesAdmin import obtener_plantas
-from routes.funcionesAdmin import obtener_comentarios_por_usuario
+from routes.funcionesAdmin import obtener_todos_los_usuarios, obtener_plantas, obtener_comentarios_por_usuario
+
 
 admin_bp = Blueprint('admin', __name__,)
 
@@ -39,6 +38,8 @@ def listar_usuarios():
 
 @admin_bp.route('/plantas', methods=['GET'])
 def listar_plantas():
+    if 'admin_id' not in session:
+        return redirect('/admin/login')
     try:
         plantas = obtener_plantas()
         return jsonify(plantas)
@@ -48,8 +49,54 @@ def listar_plantas():
 
 @admin_bp.route('/comentarios_usuario/<int:id_usuario>', methods=['GET'])
 def comentarios_usuario(id_usuario):
+    if 'admin_id' not in session:
+        return redirect('/admin/login')
     try:
         comentarios = obtener_comentarios_por_usuario(id_usuario)
         return jsonify(comentarios), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@admin_bp.route('/desactivar_planta', methods=['POST'])
+def desactivar_planta():
+    if 'admin_id' not in session:
+        return jsonify({"error": "No autorizado"}), 401
+
+    data = request.get_json()
+    id_planta = data.get('IdPlanta')
+
+    if not id_planta:
+        return jsonify({'success': False, 'error': 'Falta IdPlanta'}), 400
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE Plantas SET Activo = 0 WHERE IdPlanta = %s", (id_planta,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+@admin_bp.route('/activar_planta', methods=['POST'])
+def activar_planta():
+    if 'admin_id' not in session:
+        return jsonify({"error": "No autorizado"}), 401
+
+    data = request.get_json()
+    id_planta = data.get('IdPlanta')
+
+    if not id_planta:
+        return jsonify({'success': False, 'error': 'Falta IdPlanta'}), 400
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE Plantas SET Activo = 1 WHERE IdPlanta = %s", (id_planta,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
